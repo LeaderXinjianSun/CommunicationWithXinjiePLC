aaf360ebc49233493275775fb76719df53f153b6
sunxinjian <xinjian_sun@sina.com>
2018-01-24 14:07:07 +0800

    init

---
+++ CommunicationWithXinjiePLC/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BingLibrary.hjb.PLC;
+
+namespace CommunicationWithXinjiePLC
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool IsPLCConnect = false;
+            ushort i = 0;
+            ThingetPLC Xinjie;
+            Xinjie = new ThingetPLC();
+            while (true)
+            {
+                i++;
+                System.Threading.Thread.Sleep(10);
+                //IsPLCConnect = Xinjie.ReadSM(0);
+                //Console.WriteLine(IsPLCConnect.ToString());
+                IsPLCConnect = Xinjie.ReadM(24576);
+                if (IsPLCConnect)
+                {
+                    Xinjie.WriteD(2, "-59");
+                    double a = Xinjie.ReadD(2);
+                    Console.WriteLine(a.ToString());
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    Xinjie.ModbusDisConnect();
+                    Xinjie.ModbusInit("COM3", 19200, System.IO.Ports.Parity.Even, 8, System.IO.Ports.StopBits.One);
+                    Xinjie.ModbusConnect();
+                }
+                if (i > 9)
+                {
+                    i = 0;
+                    Console.ReadKey();
+                }
+            }
+            
+
+
+        }
+    }
+}
